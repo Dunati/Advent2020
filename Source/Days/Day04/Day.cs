4f393fa3fb6cbac306bdf0ecddb93b20844f2e8d
Dolphin Hawkins <dolphin@exitzer0.com>
2020-12-04 09:38:09 +0000

    Cleaned up the predicates a bit

--- Source/Days/Day04/Day.cs
+++ Source/Days/Day04/Day.cs
@@ -5,68 +5,47 @@ using System.Linq;
 
 class Day04 : BaseDay {
     static SortedSet<string> EyeColors = new SortedSet<string>() { "amb", "blu", "brn", "gry", "grn", "hzl", "oth", };
+
+    static bool NumberBetween(string value, int min, int max) {
+        if (int.TryParse(value, out var v)) {
+            return v >= min && v <= max;
+        }
+        return false;
+    }
+
+    static bool ValidHeight(string hgt) {
+        if (hgt.EndsWith("cm")) {
+            hgt = hgt.Substring(0, hgt.Length - 2);
+            if (int.TryParse(hgt, out int height)) {
+                return height >= 150 && height <= 193;
+            }
+            return false;
+        }
+        if (hgt.EndsWith("in")) {
+            hgt = hgt.Substring(0, hgt.Length - 2);
+            if (int.TryParse(hgt, out int height)) {
+                return height >= 59 && height <= 76;
+            }
+        }
+        return false;
+    }
+
+    static bool ValidHairColor(string hcl) {
+        if (hcl[0] == '#' && hcl.Length == 7) {
+            return int.TryParse(hcl.Substring(1), System.Globalization.NumberStyles.HexNumber, null, out var dummy);
+        }
+        return false;
+    }
+
     static Dictionary<string, Predicate<string>> AllFields = new Dictionary<string, Predicate<string>>() {
-        {
-            "byr", byr => {
-                if(int.TryParse(byr, out var year)) {
-                    return year >= 1920 && year <=2002;
-                }
-                return false;
-            }
-        },
-        {
-            "iyr", iyr => {
-                if(int.TryParse(iyr, out var year)) {
-                    return year >= 2010 && year <=2020;
-                }
-                return false;
-            }
-        },
-        {
-            "eyr", eyr => {
-                if(int.TryParse(eyr, out var year)) {
-                    return year >= 2020 && year <=2030;
-                }
-                return false;
-            }
-        },
-        {
-            "hgt", hgt => {
-                if (hgt.EndsWith("cm")) {
-                    hgt = hgt.Substring(0, hgt.Length-2);
-                    if(int.TryParse(hgt, out int height)) {
-                        return height >=150 && height <= 193;
-                    }
-                    return false;
-                }
-                if (hgt.EndsWith("in")) {
-                    hgt = hgt.Substring(0, hgt.Length-2);
-                    if(int.TryParse(hgt, out int height)) {
-                        return height >=59 && height <= 76;
-                    }
-                }
-                return false;
-            }
-        },
-        {
-            "hcl", hcl => {
-                if(hcl[0] == '#' && hcl.Length == 7) {
-                       return int.TryParse(hcl.Substring(1), System.Globalization.NumberStyles.HexNumber, null, out var dummy);
-                }
-                return false;
-            }
-        },
-        {
-            "ecl", ecl => EyeColors.Contains(ecl)
-        },
-        {
-            "pid", pid => {
-                return pid.Length == 9 && int.TryParse(pid, out int dummy);
-            }
-        },
-        {
-            "cid", cid => true
-        },
+        { "byr", byr => NumberBetween(byr, 1920, 2002) },
+        { "iyr", iyr => NumberBetween(iyr, 2010, 2020) },
+        { "eyr", eyr => NumberBetween(eyr, 2020, 2030) },
+        { "hgt", hgt => ValidHeight(hgt) },
+        { "hcl", ValidHairColor },
+        { "ecl", ecl => EyeColors.Contains(ecl) },
+        { "pid", pid =>  pid.Length == 9 && int.TryParse(pid, out int dummy) },
+        { "cid", cid => true },
     };
     public override string Run(int part, string rawData) {
         int count = 0;
