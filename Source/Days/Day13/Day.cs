86be7e7c4a68c2f40b7aa4d2b2beb10943824ccd
Dolphin Hawkins <dolphin@exitzer0.com>
2020-12-13 06:36:50 +0000

    Day 13 - 1

--- Source/Days/Day13/Day.cs
+++ Source/Days/Day13/Day.cs
@@ -4,6 +4,25 @@ using System.Linq;
 
 class Day13 : BaseDay {
     public override string Run(int part, string rawData) {
-        return "";
+
+        var lines = rawData.Lines();
+        int timestamp = lines.Select((Func<string, int>)Convert.ToInt32).First();
+        int[] buses = lines.Skip(1).First().Split(',').Where(x => x != "x").Select((Func<string, int>)Convert.ToInt32).ToArray();
+
+        int minWait = int.MaxValue;
+        int busId = 0;
+        for (int i = 0; i < buses.Length; i++) {
+            int wait = (timestamp) % buses[i];
+            if(wait != 0) {
+                wait = buses[i] - wait;
+            }
+            if (wait < minWait) {
+                minWait = wait;
+                busId = buses[i];
+            }
+        }
+
+
+        return (minWait * busId).ToString();
     }
 }
