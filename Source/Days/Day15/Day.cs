8841f9658e3c4b63777744348106649c2d5372cb
Dolphin Hawkins <dolphin@exitzer0.com>
2020-12-15 09:50:38 +0000

    Day 15

--- Source/Days/Day15/Day.cs
+++ Source/Days/Day15/Day.cs
@@ -4,6 +4,35 @@ using System.Linq;
 
 class Day15 : BaseDay {
     public override string Run(int part, string rawData) {
-        return "";
+
+        DefaultDictionary<int, int> seen = new DefaultDictionary<int, int>();
+
+        int round = 1;
+
+        var s = rawData.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+
+        foreach (int i in s.Select((Func<string,int>)Convert.ToInt32)) {
+            seen[i] = round++;
+        }
+
+        int toAdd = 0;
+        int stop = 2020;
+        if(part == 2) {
+            stop = 30000000;
+        }
+
+        while (round < stop) {
+            int nextNumber = 0;
+            if (seen.ContainsKey(toAdd)) {
+                nextNumber = round - seen[toAdd];
+            }
+            seen[toAdd] = round++;
+            toAdd = nextNumber;
+        }
+
+
+
+        return toAdd.ToString();
     }
 }
